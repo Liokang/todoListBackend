fd7a9a90a90150310a7b50111387d434fede335a
Liokang <60290160+Liokang@users.noreply.github.com>
2025-09-29 08:57:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin: "http://localhost:3000",
+        origin: "https://todo-list-front-theta.vercel.app/",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
